39e9f06bf3b1479b53a4d2b9456f764513eb267f
Liam <droidology@allianceofdroids.org.au>
2022-05-04 22:15:15 +0800

    Confirmed hardhat test works for persisted comments

--- contracts/Comments.sol
+++ contracts/Comments.sol
@@ -2,6 +2,8 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
+import "hardhat/console.sol";
+
 contract Comments {
     // Exposed data structure
     struct Comment {
@@ -13,10 +15,32 @@ contract Comments {
     }
 
     // Notify users that a comment was added
-    event CommentAddress(Comment comment);
+    event CommentAdded(Comment comment);
     // Fetch a list of comments for a topic
     function getCommments(string calldata topic) public view returns(Comment[] memory) {}
 
+    // Store each topic with its array of associated comments
+    mapping(string => Comment[]) private commentsByTopic;
+
+    // Implement getComments test
+    function getComments(string calldata topic) public view returns(Comment[] memory) {
+        return commentsByTopic[topic];
+    }
+
+    // Data struct for adding comments
+    uint32 private idCounter;
+
     // Persist a new comment
-    function addComment(string calldata topic, string calldata message) public {}
+    function addComment(string calldata topic, string calldata message) public {
+        Comment memory comment = Comment({
+            id: idCounter,
+            topic: topic,
+            creator_address: msg.sender,
+            message: message,
+            created_at: block.timestamp
+        });
+        commentsByTopic[topic].push(comment);
+        idCounter++;
+        emit CommentAdded(comment);
+    }
 }
