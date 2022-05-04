01717e901132354df8e996eb4765a64086bd145e
Liam <droidology@allianceofdroids.org.au>
2022-05-04 22:22:12 +0800

    Creating deployment module for contract & test in #1

---
+++ scripts/deploy-and-send.js
@@ -0,0 +1,25 @@
+const hre = require("hardhat");
+const { mainModule } = require("process");
+
+async function Main() {
+  // Get contract to deploy
+  const CommentsContract = await hre.ethers.getContractFactory("Comments");
+  const contract = await CommentsContract.deploy();
+
+  await contract.deployed();
+
+  const tx1 = await contract.addComment("my-blog-post", "My first comment");
+  await tx1.wait();
+
+  const tx2 = await contract.addComment("my-blog-post", "My second comment");
+  await tx2.wait();
+
+  console.log("Contract deployed to: ", contract.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
